06e2fae10589117ce417ec1fc037f679a46807e5
Vitória Teixeira <107013063+vitoriateixeiraa@users.noreply.github.com>
2024-01-29 19:21:47 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,176 +1,212 @@
-const formCreate = document.getElementById('form-create')
-const formEdit = document.getElementById('form-edit')
-const listGroupTodo = document.getElementById('list-group-todo')
+const formCreate = document.getElementById("form-create");
+const formEdit = document.getElementById("form-edit");
+const listGroupTodo = document.getElementById("list-group-todo");
 // const messageCreate = document.getElementById('message-create')
-const time = document.getElementById('time')
-const modal = document.getElementById('modal')
-const overlay = document.getElementById('overlay')
+const time = document.getElementById("time");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
 /* time elements */
-const fullDay = document.getElementById('full-day')
-const hourEl = document.getElementById('hour')
-const minuteEl = document.getElementById('minute')
-const secondEl = document.getElementById('second')
-const closeEl = document.getElementById('close')
-let editItemId
+const fullDay = document.getElementById("full-day");
+const hourEl = document.getElementById("hour");
+
+const closeEl = document.getElementById("close");
+
+const key = "81657a86ff2546c3aaa0982d22a97161";
+const urlApi = `https://crudcrud.com/api/${key}/tasks`;
 
 // check
 
-let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')): [];
-// console.log(todos);
-
-if(todos.length) showTodos()
-
-// setTodos to LocaleStorage 
-function setTodos() {
-    localStorage.setItem('list', JSON.stringify(todos))
+async function postTask(task) {
+  try {
+    const response = await fetch(urlApi, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    if (response.ok) {
+      getAll();
+      return;
+    }
+  } catch (error) {
+    showMessage(
+      "message-create",
+      "Please check communication with the api crud-crud"
+    );
+    throw error;
+  }
 }
+
+async function deleteTask(id) {
+  try {
+    const response = await fetch(`${urlApi}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      getAll();
+      return;
+    }
+  } catch (error) {
+    showMessage(
+      "message-create",
+      "Please check communication with the api crud-crud"
+    );
+    throw error;
+  }
+}
+
+async function editTask(id, task) {
+  try {
+    const response = await fetch(`${urlApi}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    if (response.ok) {
+      close();
+      getAll();
+    }
+  } catch (error) {}
+}
+
+async function getAll() {
+  try {
+    const response = await fetch(urlApi);
+    if (response.ok) {
+      const dataResponse = await response.json();
+      listGroupTodo.innerHTML = "";
+      dataResponse.forEach((item, i) => {
+        const id = item._id;
+        listGroupTodo.innerHTML += `<li ondblclick="setCompleted(${i})" class="list-group-item d-flex justify-content-between ${
+          item.completed == true ? "completed" : ""
+        }">${item.task}
+              <div class="todo-icons"><span class="opacity-50 me-2">${
+                item.time
+              }</span>
+              <img onclick=(openModal('${id}')) src="./images/edit.svg" alt="edit icon" class="edit-icon" width="25" height="25">
+              <img onclick=(deleteTask('${id}')) src="./images/delete.svg" alt="delete icon" class="delete-icon" width="25" height="25">
+              </div>
+          </li>`;
+      });
+    }
+  } catch (error) {
+    showMessage(
+      "message-create",
+      "Please check communication with the api crud-crud"
+    );
+  }
+}
+
+async function findById(id) {
+  try {
+    const response = await fetch(`${urlApi}/${id}`);
+
+    if (response.ok) {
+      const dataResponse = await response.json();
+      return dataResponse;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+getAll();
 
 //show time
 function getTime() {
-    const now = new Date()
-    const date = now.getDate() < 10 ? '0' + now.getDate(): now.getDate()
-    const month = now.getMonth() < 10 ?  '0' + (now.getMonth() + 1): now.getMonth()
-    const year = now.getFullYear()
+  const now = new Date();
+  const time = now.toLocaleTimeString();
+  const date = now.toLocaleDateString();
 
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Novembro",
+    "Dezembro",
+  ];
 
-    const hour = now.getHours() < 10 ? '0' + now.getHours(): now.getHours()
-    const minute = now.getMinutes() < 10 ? '0' + now.getMinutes(): now.getMinutes()
-    const second = now.getSeconds() < 10 ? '0' + now.getSeconds(): now.getSeconds()
+  const monthtitle = now.getMonth();
 
+  fullDay.textContent = `${date.substring(0, 2)} ${
+    months[monthtitle]
+  } ${date.substring(6, 10)}`;
 
-    const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
-    const monthtitle = now.getMonth() 
-    fullDay.textContent = `${date} ${months[monthtitle]} ${year}`
-    hourEl.textContent = `${hour}`
-    minuteEl.textContent = `${minute}`
-    secondEl.textContent = `${second}`
-    fullDay.style.color = '#bcd'
-    hourEl.style.color = '#bcd'
-    minuteEl.style.color = '#bcd'
-    secondEl.style.color = '#bcd'
-
-
-
-    return `${hour}:${minute}, ${date}.${month}.${year}`
+  hourEl.textContent = `${time}`;
+  fullDay.style.color = "#bcd";
+  hourEl.style.color = "#bcd";
+  return `${time}, ${date}`;
 }
 
-setInterval(getTime, 500)
-
-// showTodos
-function showTodos() {
-    const todos = JSON.parse(localStorage.getItem('list'))
-    listGroupTodo.innerHTML = ''
-    todos.forEach((item, i) => {
-        listGroupTodo.innerHTML += `<li ondblclick="setCompleted(${i})" class="list-group-item d-flex justify-content-between ${item.completed == true ? 'completed': ""}">${item.text}
-        <div class="todo-icons"><span class="opacity-50 me-2">${item.time}</span>
-        <img onclick=(editTodo(${i})) src="./images/edit.svg" alt="edit icon" class="edit-icon" width="25" height="25">
-        <img onclick=(deleteTodo(${i})) src="./images/delete.svg" alt="delete icon" class="delete-icon" width="25" height="25">
-        </div>
-    </li>`
-    })
-}
+setInterval(getTime, 500);
 
 // show error
 function showMessage(where, message) {
-    document.getElementById(`${where}`).textContent = message
+  document.getElementById(`${where}`).textContent = message;
 
-    setTimeout(() => {
-    document.getElementById(`${where}`).textContent = ''
-    }, 2500)
+  setTimeout(() => {
+    document.getElementById(`${where}`).textContent = "";
+  }, 2500);
 }
 
-/* <ul id="list-group-todo" class="list-group">
-            <li class="list-group-item d-flex justify-content-between">Hello World!
-            <div class="todo-icons"><span class="opacity-50 me-2">20:45, 16 April, 2023</span>
-            <img src="./images/edit.svg" alt="edit icon" class="edit-icon" width="25" height="25">
-            <img src="./images/delete.svg" alt="delete icon" class="delete-icon" width="25" height="25">
-            </div>
-        </li>
-        </ul> */
+formCreate.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = formCreate["input-create"].value.trim();
+  formCreate.reset();
+  if (todoText.length) {
+    postTask({ task: todoText, time: getTime() });
+    getAll();
+  } else {
+    showMessage("message-create", "Please enter some todo...");
+  }
+});
 
+async function openModal(id) {
+  const tasks = await findById(id);
+  const inputEdit = formEdit["input-edit"];
+  inputEdit.value = tasks.task;
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+  
+  formEdit.addEventListener("submit", handleFormSubmit);
 
-formCreate.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const todoText = formCreate['input-create'].value.trim()
-    formCreate.reset()
-    if(todoText.length) {
-        todos.push({text: todoText, time: getTime(), completed: false})
-        setTodos()
-        showTodos()
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const todoText = formEdit["input-edit"].value.trim();
+    alert(id);
+
+    if (todoText) {
+      editTask(id, { task: todoText, time: getTime() });
+      formEdit.removeEventListener("submit", handleFormSubmit);
     } else {
-        showMessage('message-create', 'Please enter some todo...')
+      showMessage("message-edit", "Please enter some todo...");
     }
-})
-
-// deleteTodo 
-
-function deleteTodo(id) {
-  const deleteTodos = todos.filter((item, i) => {
-    return i !== id 
-  })
-
-  todos = deleteTodos
-  setTodos()
-  showTodos()
-}
-
-function setCompleted(id) {
-  const completeTodos = todos.map((item, i) => {
-     if(id == i) {
-        return {...item, completed: item.completed == true ? false: true}
-     } else {
-        return {...item}
-     }
-  })   
-  todos = completeTodos
-  setTodos()
-  showTodos()
-}
-
-//editForm 
-
-formEdit.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const todoText = formEdit['input-edit'].value.trim()
-    formEdit.reset()
-    if(todoText.length) {
-        todos.splice(editItemId, 1,{text: todoText, time: getTime(), completed: false})
-        setTodos()
-        showTodos()
-        close()
-    } else {
-        showMessage('message-edit', 'Please enter some todo...')
-    }
-
-})
-
-// editTodDo
-function editTodo(id) {
-    open()
-    editItemId = id
-}
-
-
-function open() {
-    overlay.classList.remove('hidden')
-    modal.classList.remove('hidden')
-
+  }
 }
 
 function close() {
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
+overlay.addEventListener("click", close);
+closeEl.addEventListener("click", close);
 
-overlay.addEventListener('click', close)
-closeEl.addEventListener('click', close)
-
-document.addEventListener('keydown', (e) => {
-    if(e.key == 'Escape') {
-        close()
-        
-    }
-})
-
-
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    close();
+  }
+});
